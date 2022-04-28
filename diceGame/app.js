d02ad79f05b44b5ad83c3dffd22036d2f16370f5
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-04-28 11:00:35 +0100

    Update app.js

--- diceGame/app.js
+++ diceGame/app.js
@@ -3,7 +3,7 @@ var player1 = "Player 1";
 var player2 = "Player 2";
 
 // Function to change the player name
-function namesChange() {
+function nameChange() {
     player1 = prompt("Change Player1 name");
     player2 = prompt("Change player2 name");
 
